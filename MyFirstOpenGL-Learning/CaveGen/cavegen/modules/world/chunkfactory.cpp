8cfdf1e62e75c9329257599ec71d0b95da9dce2a
alephpt <alephpt@github.com>
2024-04-21 13:28:43 -0500

    time for renderdoc

--- MyFirstOpenGL-Learning/CaveGen/cavegen/modules/world/chunkfactory.cpp
+++ MyFirstOpenGL-Learning/CaveGen/cavegen/modules/world/chunkfactory.cpp
@@ -281,7 +281,7 @@ void ChunkGenerator::Normalize()
         Logger::Verbose("Calculating Normals:\n");
         Logger::Verbose("Vertices: %d\n", positions.size());
 
-        // Calculate normals for each triangle and add them to corresponding vertices
+        // // Calculate normals for each triangle and add them to corresponding vertices
         for(int i = 0; i < indices.size(); i += 3)
             {
                 glm::vec3 _v1 = positions[indices[i + 1]] - positions[indices[i]];
